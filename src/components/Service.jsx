bc68ccfd6b52cc625ab80876d64066b3849b0838
razibur88 <rajib.cit.bd@gmail.com>
2024-03-27 09:16:50 +0600

    demo

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import SerOne from "../assets/serone.png"
+import React from "react";
+import SerOne from "../assets/serone.png";
 
 const Service = () => {
   return (
     <>
-        <div className="flex flex-wrap items-center">
-            <div className="w-1/2 h-[300px]">
-                <div className="flex items-center h-[300px]">
-                <div className="">
-                <h2>Our Services add korci</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                </div>
-                </div>
+      <div className="flex flex-wrap items-center">
+        <div className="w-1/2 h-[300px]">
+          <div className="flex items-center h-[300px]">
+            <div className="">
+              <h2>Our Services add korci</h2>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </p>
             </div>
-            <div className={`bg-[url('..${SerOne}')] w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}>
-                <h2>hello</h2>
-            </div>
-            <div className={`bg-[url('..${SerOne}')] w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}>
-                <h2>hello</h2>
-            </div>
-            <div className={`bg-[url('..${SerOne}')] w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}>
-                <h2>hello</h2>
-            </div>
+          </div>
         </div>
+        <div
+          style={{ background: `url(${SerOne})` }}
+          className={`w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}
+        >
+          <h2>hello</h2>
+        </div>
+        <div
+          className={`bg-[url('..${SerOne}')] w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}
+        >
+          <h2>hello</h2>
+        </div>
+        <div
+          className={`bg-[url('..${SerOne}')] w-1/2 h-[300px] bg-no-repeat bg-cover bg-center`}
+        >
+          <h2>hello</h2>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
